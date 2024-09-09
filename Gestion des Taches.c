b2259952015d155e713657d26e53019df19277b8
oussama <outaroussama88@gmail.com>
2024-09-09 10:09:51 +0100

    Ajout du menu principal

--- Gestion des Taches.c
+++ Gestion des Taches.c
@@ -8,7 +8,25 @@ typedef struct {
     char statut[25];
 } Tache;
 
+void menuPrincipal() {
+    ligneEtoiles();
+
+    printf("Veuillez choisir une option:\n");
+    printf("1. Ajouter une tâche.\n");
+    printf("2. Modifier une tâche.\n");
+    printf("3. Supprimer une tâche.\n");
+    printf("4. Rechercher une tâche.\n");
+    printf("5. Afficher des Statistiques.\n");
+
+    ligneEtoiles();
+}
+
+void ligneEtoiles() {
+    printf("***********************************************\n");
+}
+
 int main() {
+        
 
     return 0;
 }
