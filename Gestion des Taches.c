28f449633e10e7baa88a9bf2cc9d5b8cc30beb64
oussama <outaroussama88@gmail.com>
2024-09-09 10:01:57 +0100

    Definition de la structure Tache

--- Gestion des Taches.c
+++ Gestion des Taches.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+typedef struct {
+    int id;
+    char titre[100];
+    char description[1000];
+    char deadline[10];
+    char statut[25];
+} Tache;
+
 int main() {
 
     return 0;
