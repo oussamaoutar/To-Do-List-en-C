e1ccc0e1c561839b21ed9af67fe9ace65b40a73b
oussama <outaroussama88@gmail.com>
2024-09-09 15:07:22 +0100

    Ajout Modification d'une tâche

--- Gestion des Taches.c
+++ Gestion des Taches.c
@@ -25,6 +25,8 @@ int main() {
         printf("*Tapper autre chiffre pour quitter*\n");
         scanf("%d", &choix);
         switch(choix) {
+
+            // Ajout d'une tâche
             case 1: {
                 int id;
                 char titre[100];
@@ -52,9 +54,34 @@ int main() {
                 count++;
                 printf("Tâche Ajoutée avec Succès!\n");
             } break;
+
+            // Modification d'une tâche
             case 2: {
-                
+                if(!count) printf("Il n'y a aucune tâche à modifier.\n");
+                else {
+                    int choix;
+                    printf("Veuillez choisir l'ID du tâche à modifier:\nID | Titre | Description | Deadline | Statut\n");
+                    for(int i=0; i<count; i++) {
+                        printf("%d | %s | %s | %s | %s\n", taches[i].id, taches[i].titre, taches[i].description, taches[i].deadline, taches[i].statut);
+                    }
+                    scanf("%d", &choix);
+                    if(choix>count || choix<0) printf("Choix invalide!");
+                    else {
+                        printf("Nouveau Description ===> ");
+                        scanf(" %[^\n]s", T[choix].description);
+                        printf("Nouveau Deadline ===> ");
+                        scanf(" %[^\n]s", T[choix].deadline);
+                        printf("Nouveau Statut ===> ");
+                        scanf(" %[^\n]s", T[choix].statut);
+                        printf("Tâche Modifiée avec succès!\n");
+                    }
+                }
             }; break;
+
+            case 3: {
+
+            }; break;
+
         }
     } while(choix >= 1 && choix <= 5);
 
